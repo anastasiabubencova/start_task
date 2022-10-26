bba312379d9cb3a450e253aaeb0883a9c5ab67dc
anastasia <anastasiabubencova6@gmail.com>
2022-10-26 15:34:39 +0300

    26.10.2022 работа с файлом hdf5 (странные результаты по памяти)

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from datetime import datetime
 import os
 from matplotlib import pyplot as plt
 import h5py
+import warnings
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
+
+
 
 #import datatable as dt
 #from mbf_pandas_msgpack import to_msgpack, read_msgpack
@@ -48,9 +53,20 @@ def column(column_type, length) :
         return string_column(length)
 
 
-def write_hdf(ds) :
-    new_ds = pd.DataFrame([ds[i].astype(str) for i in ds.columns])
-    print(new_ds)
+def write_hdf(filename, ds) :
+    ds = pd.concat([pd.DataFrame(ds.columns), ds], axis=1)
+    new_ds = pd.DataFrame([ds[i].astype('string') for i in ds.columns]).T.T
+    with h5py.File(filename, 'w') as f:
+        f.create_dataset('dataset', data=new_ds.values)
+
+def read_hdf(filename) :
+    with h5py.File(filename, "r") as f:
+        a_group_key = list(f.keys())[0]
+        readed_ds = h5py.Dataset.asstr(f[a_group_key])
+        ds = pd.DataFrame(list(readed_ds[1:]))
+        ds.columns = list(readed_ds[0])
+        return ds
+
 
 
 def main() :
@@ -71,12 +87,8 @@ def main() :
     ds.to_csv('results/dataset.csv')
     start_feather = datetime.now()
     ds.reset_index(drop=True).to_feather('results/dataset.feather')
-
     start_hdf = datetime.now()
-    ds.to_hdf('results/dataset.h5', key='df')
-
-
-
+    write_hdf('results/dataset.hdf5', ds)
     start_msgpack = datetime.now()
     #to_msgpack('results/dataset.msg', ds)
     start_parquet = datetime.now()
@@ -84,7 +96,6 @@ def main() :
     start_pickle = datetime.now()
     ds.to_pickle('results/dataset.pkl')
     start_jay = datetime.now()
-    #ds_dt = dt.Da
     #ds.to_jay("results/dataset.jay")
     end_time = datetime.now()
 
@@ -101,7 +112,7 @@ def main() :
     start_feather = datetime.now()
     ds = pd.read_feather('results/dataset.feather')
     start_hdf = datetime.now()
-    ds = pd.read_hdf('results/dataset.h5', key='df')
+    ds = read_hdf('results/dataset.hdf5')
     start_msgpack = datetime.now()
     #read_msgpack('results/dataset.msg', ds)
     start_parquet = datetime.now()
@@ -118,19 +129,18 @@ def main() :
                            (end_time - start_jay).microseconds]
 
     result['size'] = [os.stat('results/dataset.csv').st_size, os.stat('results/dataset.feather').st_size,
-                      os.stat('results/dataset.h5').st_size, 0, os.stat('results/dataset.gzip').st_size,
+                      os.stat('results/dataset.hdf5').st_size, 0, os.stat('results/dataset.gzip').st_size,
                       os.stat('results/dataset.pkl').st_size, 0]
 
     return result
 
 
 if __name__ == '__main__' :
-    '''result = pd.DataFrame(columns=['file', 'save_time', 'load_time', 'size'])
+    result = pd.DataFrame(columns=['file', 'save_time', 'load_time', 'size'])
     for i in range(100) :
         result = pd.concat([result, main()])
 
     print(result.groupby('file', as_index=False).mean())
-    #result.groupby('file', as_index=False).mean().plot.bar(x='file', y='save_time')
 
     fig = plt.figure(figsize=(20,10))
     plt.subplot(1, 3, 1)
@@ -142,35 +152,18 @@ if __name__ == '__main__' :
     plt.subplot(1, 3, 3)
     plt.bar(result['file'].values, result['size'].values)
     plt.title('size')
-    plt.savefig('results/res1.png')'''
-
-    '''strList = ['int', 'float', 'string', 'boolean']
-    asciiList = [n.encode("ascii", "ignore") for n in strList]
-    h5File = h5py.File('xxx.h5', 'w')
-    h5File.create_dataset('xxx', (len(asciiList), 1), 'S10', asciiList)
-    h5File.flush()
-    h5File.close()
-
-    with h5py.File('xxx.h5', "r") as f:
-        print("Keys: %s" % f.keys())
-        a_group_key = list(f.keys())[0]
-        print(type(f[a_group_key]))
-        data = list(f[a_group_key])
-        data = list(f[a_group_key])
-        ds_obj = f[a_group_key]  # returns as a h5py dataset object
-        print(ds_obj)
-        ds_arr = f[a_group_key][()]  # returns as a numpy array
-        print(ds_arr)'''
+    plt.savefig('results/res1.png')
 
 
-    ds = pd.DataFrame()
+
+    '''ds = pd.DataFrame()
     columns_num = 256
     rows_num = 256
 
     for i in range(columns_num) :
         type = random.randint(0, 2)
         ds['col' + str(i) + "_" + types[type]] = column(type, rows_num)
-        ds['col' + str(i) + "_" + types[type]] = ds['col' + str(i) + "_" + types[type]].astype(types[type])
+        ds['col' + str(i) + "_" + types[type]] = ds['col' + str(i) + "_" + types[type]].astype(types[type])'''
 
-    #print(ds)
-    write_hdf(ds)
+    #write_hdf(ds)
+    #read_hdf()
